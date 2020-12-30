91b0f3a24084ef53e627ec65fc77046fae2ecab0
nevinhall <46722804+nevinhall@users.noreply.github.com>
2020-12-30 19:45:54 +0000

    fin

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,88 @@
 import React, { useState } from 'react';
 import { Card, Button, Container, Form, Jumbotron, Modal, Accordion } from 'react-bootstrap';
 
+import '../css/images.css'
+
 import rock from "../images/rock.png"
 import scissors from "../images/scissors.png"
 import paper from "../images/paper.png"
+import questionMark from "../images/questionMark.png"
+import computer from "../images/computer.png"
+import { useStore } from 'react-redux';
+
 
 
 const Main = () => {
     const [computerVal, setComputerVal] = React.useState(0);
-    const [userVal, setUserVal] = React.useState(0);
-    const [show, setShow] = React.useState(false);
+    const [userTurn, setUserTurn] = React.useState(0);
+
+    const [userScore, setUserScore] = React.useState(0);
+    const [computerScore, setComputerScore] = React.useState(0);
 
 
+    const genScore = (props) => {
 
+        setUserTurn(1)
 
-
-    const genScore = () => {
-        console.log("t");
-        setShow(true)
         setComputerVal(Math.floor(Math.random() * 3))
+
+        if(userTurn)
+             matchHuman(props)
+        else
+            matchComputer(props)
+
+    }
+
+    const matchComputer = (props) => {
+        if (props == computerVal) {
+            setComputerScore(computerScore +1)
+            console.log("winner computer: " + computerScore);
+        }
+        setUserTurn(1)
     }
 
 
+    const matchHuman = (props) => {
+        if (props == computerVal) {
+            setUserScore(userScore +1)
+            console.log("winner human: " + userScore);
+        }
 
+        setUserTurn(0)
+    }
 
     return (
         <div>
-            <img src={rock}  />
-            <img src={scissors}  />
-            <img src={paper}  />
+            {userTurn ? <img src={questionMark} className="image" />: <img src={computer} className="image" /> }
 
-            {show ? computerVal : 0}
-            <Button onClick={genScore}>Play</Button>
+
+
+            <p>Choose Your Weapon</p>
+
+            <img src={rock} className="image" onClick={() => genScore(0)} />
+            <img src={scissors} className="image" onClick={() => genScore(1)} />
+            <img src={paper} className="image" onClick={() => genScore(2)} />
         </div>
 
     );
 }
 
 export default Main;
+
+
+
+// {console.log(userTurn)}
+// {userTurn ? <img src={questionMark} className="image" />: (() => {
+//     switch (computerVal) {
+//         case 0:
+            
+//             return <img src={rock} className="image" />
+
+//         case 1:
+            
+//             return <img src={scissors} className="image" />
+//         case 2:
+         
+//             return <img src={paper} className="image" />
+//     }
+// })()  }
